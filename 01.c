f24fa4c0a5fab30ef59f42838172a1b62b3dd3ef
firminolussau <90766368+firminolussau@users.noreply.github.com>
2021-09-17 16:31:01 +0000

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+ int main() {
+	  float lado1, lado2, Area;
+	      printf ("Digite a primeira e a segunda area do quadrado\n");
+	        scanf ("%f%f",&lado1,&lado2);
+	 
+	            Area = lado1 * lado2;
+	         printf ("A area do quadrado e de %f m2",Area);
+	     return 0;
+    }
