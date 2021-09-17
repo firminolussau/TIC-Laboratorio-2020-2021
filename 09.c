ebda288d063d04457fb730dba446a4ab86a43147
firminolussau <90766368+firminolussau@users.noreply.github.com>
2021-09-17 16:39:44 +0000

    Add files via upload

---
+++ 09.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+float P_saco, Quant_F;
+printf("Peso do saco: ");
+scanf ("%d",&P_saco);
+printf("quantidade de ração fornecida: ");
+scanf ("%d",&Quant_F);
+Quant_F *= 10;
+P_saco -=  * 0,001;
+printf("Depois de 5 dias o saco terá %d Kg",PesoSaco);
+return 0;
+}
+
